87b6c2b16b99c2771b5cf149b74a3c40643e4371
Wael Elsafty <waelelsafty07@gmail.com>
2023-05-24 18:03:15 +0300

    Fix Eslinter

--- src/modules/env.js
+++ src/modules/env.js
@@ -1,4 +1,3 @@
 const movieApi = 'https://api.tvmaze.com/';
-const invo =
-  'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/ElVEaIKZrRGXf09GE8cT/';
+const invo = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/ElVEaIKZrRGXf09GE8cT/';
 export { movieApi, invo };
